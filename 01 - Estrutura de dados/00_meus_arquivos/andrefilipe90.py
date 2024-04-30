5b43b1cfdc5094a257b4e0302851504dbaa8b5a5
andrefilipe90 <andrefilipe90@gmail.com>
2024-04-30 14:49:59 -0300

    UPDATE: Banco.rev02.cadastrar_conta

--- 01 - Estrutura de dados/00_meus_arquivos/andrefilipe90.py
+++ 01 - Estrutura de dados/00_meus_arquivos/andrefilipe90.py
@@ -5,7 +5,8 @@ def limpa_tela():
 
 limpa_tela()
 
-nome_correntista = False
+conta_ativa = {}
+usuario_ativo = {}
 lista_usuarios = [{"cpf":"05843914712",
                    "nome":"André Filipe Guering de Mattos",
                    "nascimento":"14/05/1990",
@@ -34,39 +35,55 @@ menu = f"""
 
 => """
 
-def cadastrar_usuario():
+def cadastrar_usuario(cpf):
     limpa_tela()
-    #definir atividades
-    temp_cpf = str(input("informe o CPF:"))
-    for usuario in lista_usuarios:
-        #habilitar validação de CPF com regras de existencia de CPF.
-        if usuario["cpf"] == temp_cpf:
-            print("CPF já registrado no sistema.")
-            # habilitar reescrever e apagar conta.
-            return
-        if len(temp_cpf) < 11:
-            print("CPF muito curto.")
-            return
+    temp_cpf = teste_cpf(cpf)
+    if temp_cpf == cpf:
+        return
     temp_nome = input("Informe o nome:")
     temp_nascimento = input("informe a data de nascimento dd/mm/aaaa:")
     temp_endereco = input("informe o endereço: rua, nº - bairro - cidade/ES:")
     temp_dict = {"cpf":temp_cpf,"nome":temp_nome,"nascimento":temp_nascimento,"endereço":temp_endereco}
     lista_usuarios.append(temp_dict)
-
+    criar_conta = input("gostaria de criar uma conta? [s]sim ou [n]não:")
+    if criar_conta == "s":
+        cadastrar_conta(agencia="0001",cpf=temp_cpf)
+        return
+    elif criar_conta == "n":
+        return
+    else:
+        print("opção não encontrada, retornando ao menu:")
     return
 
-def cadastrar_conta():
+def cadastrar_conta(cpf,agencia):
+    limpa_tela()
+    temp_agencia = agencia
+    cpf_valido = teste_cpf(cpf)
+    if cpf_valido == cpf:
+        for usuario in lista_usuarios:
+            if usuario["cpf"] == cpf:
+                temp_conta= 1+ int(lista_contas[-1]["conta"])
+                temp_cheque_especial = int(input("informar limite cheque especial:"))
+                lista_contas.append = {"conta":temp_conta,
+                                       "agencia":temp_agencia,
+                                       "cpf":cpf,
+                                       "saldo":0,
+                                       "cheque_especial":temp_cheque_especial}
+    else:
+        temp_cpf = str(input("Não encontrado, informe o CPF:"))
+        cadastrar_usuario(temp_cpf)
+    #definir atividades
+    return
+
+def depositar(agencia,conta):
     limpa_tela()
     #definir atividades
-    print(lista_contas[0],lista_usuarios[0]["cpf"])
+    valor = float(input("informe o valor para saque:"))
+    if valor > 0:
+        print("valor")
     return
 
 def sacar():
-    limpa_tela()
-    #definir atividades
-    return
-
-def depositar():
     limpa_tela()
     #definir atividades
     return
@@ -83,14 +100,54 @@ def saindo(nome):
     else:
         print("Obrigado por usar o banco X.")
 
+def teste_cpf(cpf):
+    for usuario in lista_usuarios:
+    #habilitar validação de CPF com regras de existencia de CPF.
+        if usuario["cpf"] == cpf:
+            print("CPF já registrado no sistema.")
+            # habilitar reescrever e apagar conta.
+            return cpf
+        if len(cpf) != 11:
+            print("CPF Invalido.")
+            return
+        
+def validar_conta(agencia,conta):
+    for contas in lista_contas:
+        if int(conta) == contas["conta"]:
+            limpa_tela()
+            print("conta encontrada")
+            for usuario in lista_usuarios:
+                if contas["cpf"] == usuario["cpf"]:
+                    global usuario_ativo
+                    usuario_ativo = usuario
+            global conta_ativa
+            conta_ativa = contas
+            return True
+    return False    
+    
+def atualiza_conta(conta_ativa):
+    for conta in lista_contas:
+        if conta_ativa["conta"] == conta["conta"]:
+            conta.update(conta_ativa)
 while True:
     opcao = input(menu)
     if opcao == "1":
-        cadastrar_usuario()
+        temp_cpf = str(input("informe o CPF:"))
+        cadastrar_usuario(temp_cpf)
     elif opcao == "2":
-        cadastrar_conta()
+        temp_cpf = input("Informe o CPF a cadastrar conta:")
+        cadastrar_conta(temp_cpf,"0001")
     elif opcao == "3":
-        depositar()
+        if conta_ativa:
+            depositar(conta_ativa["agencia"],conta_ativa["conta"])
+        else:
+            agencia = input("informe o numero da agencia com 4 digitos:")
+            conta = input("informe o numero da conta:")
+            validade_conta = validar_conta(agencia,conta)
+            if  validar_conta == True:
+                depositar(conta_ativa["agencia"],conta_ativa["conta"])
+            elif validar_conta == False:
+                print("Tente Novamente, conta não encontrada.")
     elif opcao == "4":
         sacar()
     elif opcao == "5":
@@ -103,5 +160,4 @@ while True:
         for conta in lista_contas:
             print(conta)
     elif opcao == "6":
-        saindo(nome_correntista)
         break
--- 01 - Estrutura de dados/00_meus_arquivos/banco.py
+++ 01 - Estrutura de dados/00_meus_arquivos/banco.py
@@ -4,13 +4,11 @@
 # cadastrar usuario (cliente) e cadastrar conta bancaria (conta para esse cliente)
 # armazenar usuarios em uma lista, composto por nome, data de nascimento, cpf e endereco
 # endereço(str) em formato string Rua, numero - bairro - cidade/sigla estado. cpf só o numero
-
+# cadastrar_usuario: não pode haver numeros duplicados de CPF (string)para vincular um
+# usuario a uma conta, filtrar lista por CPF.
 '''rev01:
 
 as funcoes devem fazer passagem de argumentos por posicao e nomeados (variar os casos)
-
-cadastrar_usuario: não pode haver numeros duplicados de CPF (string)para vincular um
-usuario a uma conta, filtrar lista por CPF.
 
 Saque: argumentos: keyword only: saque(saldo="variavel_saldo"...)
 Saque: usar argumentos: (saldo, valor, extrato, limite, numero_saques,limite_saques)
@@ -25,6 +23,17 @@ extrato: positional only e keywork only: extrato(saldo,extrato="variavel_extrato
 conta em uma lista, coposta por agencia, numero da conta e usuario, contas sequenciais
 começa em 1, agencia fixo 0001,cada usuario pode ter mais de uma conta, mas uma conta não
 pode ser de dois usuarios, não pode haver conta sem usuario.
+
+lista_usuarios = [{"cpf":"05843914712",
+                   "nome":"André Filipe Guering de Mattos",
+                   "nascimento":"14/05/1990",
+                   "endereço":"Rua Roberto Fritzgerald Kennedy,40 - Cavaleiros - Macaé/RJ"}]
+lista_contas = [{"conta":1,
+                 "agencia":"0001",
+                 "cpf":"05843914712",
+                 "saldo":100,
+                 "cheque_especial":100}] #definir tipo
+                 
 '''
 
 #def saque(saldo="saldo",valor="valor",extrato="extrato",limite="limite",numero_saques="",
