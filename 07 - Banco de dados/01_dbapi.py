eadbe76cf7154e2526550d0f0d0b81b965861f8c
andrefilipe90 <andrefilipe90@gmail.com>
2024-05-21 16:09:49 -0300

    UPDATE: teste.sqlite

--- 07 - Banco de dados/01_dbapi.py
+++ 07 - Banco de dados/01_dbapi.py
@@ -3,14 +3,50 @@ from pathlib import Path
 
 ROOT_PATH = Path(__file__).parent
 
-conexao = sqlite3.connect(ROOT_PATH / "clientes.sqlite")
-cursor = execute(
-    "CREATE TABLE clientes (id INTERGER PRIMARY KEY AUTOINCREMENT, nome VARCHAR(100), email VARCHAR(150))"
-)
-# cursor = conexao.cursor()
-# cursor.row_factory = sqlite3.Row
+conexao = sqlite3.connect(ROOT_PATH / "teste.sqlite")
+cursor = conexao.cursor()
 
 
+def criar_tabela(conexao, cursor):
+    cursor.execute(
+        "CREATE TABLE clientes (id INTEGER PRIMARY KEY AUTOINCREMENT, nome VARCHAR(100), email VARCHAR(150)"
+    )
+    conexao.commit()
+
+
+def adicionar_valores(conexao, cursor, nome, email):
+    info = (nome, email)
+    cursor.execute("INSERT INTO clientes (nome, email) VALUES (?, ?)", info)
+    conexao.commit()
+
+
+def alterar_valores(conexao, cursor, id, nome, email):
+    info = (nome, email, id)
+    cursor.execute("UPDATE clientes SET nome=?, email=? where id=?;", info)
+    conexao.commit()
+
+
+def remover_valores(conexao, cursor, id):
+    info = (id,)
+    cursor.execute("DELETE FROM clientes WHERE id=?", info)
+    conexao.commit()
+
+
+def adicionar_muitos(conexao, cursor, dados):
+    cursor.executemany("INSERT INTO clientes (nome, email) VALUES (?, ?)", dados)
+    conexao.commit()
+
+
+dados = [
+    ("André", "andre@gmail.com"),
+    ("Sophie mattos", "Sophiemattos@gmail.com"),
+    ("Denise", "denise@gmail.com"),
+    ("Marielle", "marielle@gmail.com"),
+]
+
+adicionar_muitos(conexao, cursor, dados)
+# remover_valores(conexao, cursor, 2)
+# alterar_valores(conexao, cursor, 1, "André Mattos", "andrefilipe90@gmail.com")
 # def criar_tabela(conexao, cursor):
 #     cursor.execute(
 #         "CREATE TABLE clientes (id INTEGER PRIMARY KEY AUTOINCREMENT, nome VARCHAR(100), email VARCHAR(150))"
